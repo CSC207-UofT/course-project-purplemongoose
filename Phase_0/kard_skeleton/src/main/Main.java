6bbb203a7249b8d669fd31068aa2147b39384ebb
affixrevy <arthurerconggao@gmail.com>
2021-10-14 14:34:28 -0400

    Added documentation to Main and ContactController

--- Phase_0/kard_skeleton/src/main/Main.java
+++ Phase_0/kard_skeleton/src/main/Main.java
@@ -29,20 +29,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input;
 
-        System.out.println("\n" +
-                "\n" +
-                "██╗  ██╗ █████╗ ██████╗ ██████╗ \n" +
-                "██║ ██╔╝██╔══██╗██╔══██╗██╔══██╗\n" +
-                "█████╔╝ ███████║██████╔╝██║  ██║\n" +
-                "██╔═██╗ ██╔══██║██╔══██╗██║  ██║\n" +
-                "██║  ██╗██║  ██║██║  ██║██████╔╝\n" +
-                "╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ \n");
+        System.out.println("""
+
+
+                ██╗  ██╗ █████╗ ██████╗ ██████╗\s
+                ██║ ██╔╝██╔══██╗██╔══██╗██╔══██╗
+                █████╔╝ ███████║██████╔╝██║  ██║
+                ██╔═██╗ ██╔══██║██╔══██╗██║  ██║
+                ██║  ██╗██║  ██║██║  ██║██████╔╝
+                ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝\s
+                """);
         System.out.println("Press enter to continue...");
-        try{System.in.read();}
-        catch(Exception e){}
+        try {
+            System.in.read();
+        } catch (Exception e) {
+        }
 
         eventLoop:
-        while(true) {
+        while (true) {
             System.out.println("Type 'add' to add users to your contacts list");
             System.out.println("Type 'remove' to remove users from your contacts list");
             System.out.println("Type 'display' to display your contacts list");
@@ -51,20 +55,10 @@ public class Main {
             input = sc.nextLine();
             switch (input) {
                 case "add":
-                    System.out.println("Type the name of the person you want to add; type 'back' to exit");
-                    input = sc.nextLine();
-                    while (!input.equals("back")) {
-                        cc.addContactMainFrame(input);
-                        input = sc.nextLine();
-                    }
+                    addPerson(cc, sc);
                     break;
                 case "remove":
-                    System.out.println("Type the name of the person you want to remove; type 'back' to exit");
-                    input = sc.nextLine();
-                    while (!input.equals("back")) {
-                        cc.removeContactMainFrame(input);
-                        input = sc.nextLine();
-                    }
+                    removePerson(cc, sc);
                     break;
                 case "display":
                     System.out.println(user.getContact()); //temporary; should be part of ContactController
@@ -75,11 +69,40 @@ public class Main {
                     System.out.println("Command not recognized... Try again\n");
                     break;
             }
-            
         }
-
     }
 
+    /**
+     * Remove a person from the mainframe using the contact controller
+     *
+     * @param cc the contact controller instance
+     * @param sc the scanner used for user input
+     */
+    private static void removePerson(ContactController cc, Scanner sc) {
+        String input;
+        System.out.println("Type the name of the person you want to remove; type 'back' to exit");
+        input = sc.nextLine();
+        while (!input.equals("back")) {
+            cc.removeContactMainFrame(input);
+            input = sc.nextLine();
+        }
+    }
+
+    /**
+     * Add a person to the mainframe using the contact controller
+     *
+     * @param cc the contact controller instance
+     * @param sc the scanner used for user input
+     */
+    private static void addPerson(ContactController cc, Scanner sc) {
+        String input;
+        System.out.println("Type the name of the person you want to add; type 'back' to exit");
+        input = sc.nextLine();
+        while (!input.equals("back")) {
+            cc.addContactMainFrame(input);
+            input = sc.nextLine();
+        }
+    }
 }
 
 
