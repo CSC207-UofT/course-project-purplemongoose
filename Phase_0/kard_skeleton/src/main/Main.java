4a7a882fbbf0bbfc8005cd09ad5c02fb96ef92f0
affixrevy <arthurerconggao@gmail.com>
2021-10-14 18:14:24 -0400

    Extracted methods from main and added 'add person' method

--- Phase_0/kard_skeleton/src/main/Main.java
+++ Phase_0/kard_skeleton/src/main/Main.java
@@ -13,21 +13,12 @@ public class Main {
         // Database for storing Persons
         MainFrame mf = new MainFrame();
 
-        // Assume these are people the user can find and add
-        Person p1 = new Person("Jack Daniels", "123-456-6789", "jack.daniel@whiskey.com");
-        Person p2 = new Person("Bob", "111-222-3333", "bob@thebuilder.com");
-        Person p3 = new Person("Jenn Ifer", "121-323-4343", "jennifer@mail.com");
-
-        //Adding users to the mf
-        mf.addEntity(p1, "Jack Daniels");
-        mf.addEntity(p2, "Bob");
-        mf.addEntity(p3, "Jenn Ifer");
+        buildInitialContacts(mf);
 
         PersonalUser user = new PersonalUser();
         ContactController cc = new ContactController(mf, user);
 
         Scanner sc = new Scanner(System.in);
-        String input;
 
         System.out.println("""
 
@@ -45,9 +36,39 @@ public class Main {
         } catch (Exception e) {
         }
 
+        events(user, cc, sc, mf);
+    }
+
+    /**
+     * Adds some initial contacts to the database for testing purposes
+     *
+     * @param mf Mainframe, the database for the program
+     */
+    private static void buildInitialContacts(MainFrame mf) {
+        // Assume these are people the user can find and add
+        Person p1 = new Person("Jack Daniels", "123-456-6789", "jack.daniel@whiskey.com");
+        Person p2 = new Person("Bob", "111-222-3333", "bob@thebuilder.com");
+        Person p3 = new Person("Jenn Ifer", "121-323-4343", "jennifer@mail.com");
+
+        //Adding users to the mf
+        mf.addEntity(p1, "Jack Daniels");
+        mf.addEntity(p2, "Bob");
+        mf.addEntity(p3, "Jenn Ifer");
+    }
+
+    /**
+     * todo add!
+     *
+     * @param user
+     * @param cc
+     * @param sc
+     */
+    private static void events(PersonalUser user, ContactController cc, Scanner sc, MainFrame mf) {
+        String input;
         eventLoop:
         while (true) {
             System.out.println("Type 'add' to add users to your contacts list");
+            System.out.println("Type 'add person' to add a user to the database");
             System.out.println("Type 'remove' to remove users from your contacts list");
             System.out.println("Type 'display' to display your contacts list");
             System.out.println("Type 'quit' to exit the program");
@@ -55,13 +76,16 @@ public class Main {
             input = sc.nextLine();
             switch (input) {
                 case "add":
-                    addPerson(cc, sc);
+                    addContact(cc, sc);
                     break;
                 case "remove":
-                    removePerson(cc, sc);
+                    removeContact(cc, sc);
                     break;
                 case "display":
                     System.out.println(user.getContact()); //temporary; should be part of ContactController
+                    break;
+                case "add person":
+                    addPerson(mf, sc);
                     break;
                 case "quit":
                     break eventLoop;
@@ -72,13 +96,13 @@ public class Main {
         }
     }
 
-    /**
+    /** todo fix: does not add to database
      * Remove a person from the mainframe using the contact controller
      *
      * @param cc the contact controller instance
      * @param sc the scanner used for user input
      */
-    private static void removePerson(ContactController cc, Scanner sc) {
+    private static void removeContact(ContactController cc, Scanner sc) {
         String input;
         System.out.println("Type the name of the person you want to remove; type 'back' to exit");
         input = sc.nextLine();
@@ -88,13 +112,13 @@ public class Main {
         }
     }
 
-    /**
+    /** todo fix: does not remove from database
      * Add a person to the mainframe using the contact controller
      *
      * @param cc the contact controller instance
      * @param sc the scanner used for user input
      */
-    private static void addPerson(ContactController cc, Scanner sc) {
+    private static void addContact(ContactController cc, Scanner sc) {
         String input;
         System.out.println("Type the name of the person you want to add; type 'back' to exit");
         input = sc.nextLine();
@@ -102,6 +126,28 @@ public class Main {
             cc.addContactMainFrame(input);
             input = sc.nextLine();
         }
+    }
+
+    /**
+     * Add a new person to the database
+     *
+     * @param mf
+     * @param sc
+     */
+    private static void addPerson(MainFrame mf, Scanner sc) {
+        System.out.println("Enter the name of the person to add:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter the phone number of the person to add:");
+        String phone = sc.nextLine();
+
+        System.out.println("Enter the email of the person to add:");
+        String email = sc.nextLine();
+
+        Person newPerson = new Person(name, phone, email);
+        mf.addEntity(newPerson, name);
+
+        System.out.println(name + " has been added to the database!");
     }
 }
 
