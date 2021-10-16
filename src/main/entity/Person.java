11e3dc594fc4980689ad0cf82ff3fc629fff320c
Affixrevy <arthurerconggao@gmail.com>
2021-10-15 22:58:53 -0400

    Edited Person.java. Previous version used an experimental feature

--- src/main/entity/Person.java
+++ src/main/entity/Person.java
@@ -1,6 +1,16 @@
 package entity;
 
-public record Person(String name, String phone, String email) implements Client {
+public class Person implements Client {
+
+    private final String name;
+    private final String phone;
+    private final String email;
+
+    public Person(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
