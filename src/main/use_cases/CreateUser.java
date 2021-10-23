d40b2a11da14e1cb9cbd7389a3dab7bb0163a2cd
Meric Sila Taskin <sila.taskin@mail.utoronto.ca>
2021-10-23 00:10:56 -0400

    edited use cases

--- src/main/use_cases/CreateUser.java
+++ src/main/use_cases/CreateUser.java
@@ -3,18 +3,14 @@ package use_cases;
 import entity.Person;
 import entity.PersonalUser;
 
-import java.util.Objects;
-
 public class CreateUser {
 
-    public boolean create(String name, String phone, String email, String kind) {
+    public void create(String name, String phone, String email, String kind) {
 
         // kind is p if this is a PersonalUser and the first letter of the type of organization if CorporateUser.
         // right now we only have Business as a child class of Organization so only that case will be implemented.
 
-        // a user should only be added to the database if it does not already exist
-        // so this method should first check if this user is already in the db and if yes, return false
-        // otherwise proceed
+        // assuming this user is not already in the database
 
         if (kind.equals("p")) {
             PersonalUser pu = new PersonalUser();
@@ -28,6 +24,5 @@ public class CreateUser {
             }
             // add cases for other kinds accordingly
         }
-        return true;
     }
 }
--- src/main/use_cases/ModifyContacts.java
+++ src/main/use_cases/ModifyContacts.java
@@ -1,9 +1,19 @@
 package use_cases;
 
+import entity.Client;
+import entity.User;
+
 public class ModifyContacts {
 
-    public boolean add(){
-        // only add if contact does not already exist
+    public void add(User user, Client contact){
+        // assuming the contact does not already exist in the contact list of the user
 
+        user.addContact(contact);
+    }
+
+    public void remove(User user, Client contact){
+        // assuming the contact is in the contact list of the user
+
+        user.removeContact(contact);
     }
 }
