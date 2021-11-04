3f8a0d198d29e813fb7853e4f56b228bb7294ed1
vzhang1112 <vzhang1112@outlook.com>
2021-11-04 11:28:15 -0400

    Added documentation to LoginAuth

--- src/main/usecase/LoginAuth.java
+++ src/main/usecase/LoginAuth.java
@@ -4,8 +4,20 @@ import state.AppState;
 import database.AccountGateway;
 
 public class LoginAuth {
+    /**
+     * Authenticates login credentials, initializes the account gateway for this user
+     */
     AccountGateway ag = new AccountGateway();
 
+    /**
+     * Checks if a UUID has already been associated to this username
+     *
+     * If there is already an associated username, then return ture and proceed.
+     *
+     * @param username string for username
+     * @param password string for password
+     * @return true if username, UUID, and password all match
+     */
     public boolean requestLogin(String username, String password) {
         String uuid = ag.authAccountData(username, password);
         if (uuid == null) {
