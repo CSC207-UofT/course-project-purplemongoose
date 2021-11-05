bfc62d4b3bff79cf3a4286802f018fd2616b410a
Meric Sila Taskin <sila.taskin@mail.utoronto.ca>
2021-11-05 05:56:27 -0400

    continued ProfilesTest

--- src/test/java/entity/accounts/ProfilesTest.java
+++ src/test/java/entity/accounts/ProfilesTest.java
@@ -1,4 +1,17 @@
 package entity.accounts;
 
+import entity.datafiles.Email;
+import entity.datafiles.Name;
+import entity.datafiles.Phone;
+import entity.profiles.Business;
+import entity.profiles.Organization;
+import entity.profiles.Person;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ProfilesTest {
 }
