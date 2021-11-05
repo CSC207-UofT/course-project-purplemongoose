3b8da4331d57c26af1b756096024524f3ebb10ec
Meric Sila Taskin <sila.taskin@mail.utoronto.ca>
2021-11-05 06:10:05 -0400

    finalized ProfileUseCasesTest

--- src/test/java/entity/accounts/ProfileUseCasesTest.java
+++ src/test/java/entity/accounts/ProfileUseCasesTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import usecase.ProfileUseCases;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
