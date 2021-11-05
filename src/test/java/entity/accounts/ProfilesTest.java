78ca40c3539ec6e1f80802e91662b8c3110f42cc
Meric Sila Taskin <sila.taskin@mail.utoronto.ca>
2021-11-05 06:04:58 -0400

    finished ProfilesTest

--- src/test/java/entity/accounts/ProfilesTest.java
+++ src/test/java/entity/accounts/ProfilesTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
