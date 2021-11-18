0ec4b72d674dfacaf4df7168a6e2cfd3dec7ee7e
Stewart Chandler <stewart.chandler@student.tdsb.on.ca>
2021-11-17 23:38:30 -0500

    added changes to SQLiteDataBaseFile to create a directory if it doesn't exist

--- kard-server/src/main/database/SQLite/SQLiteDataBaseFile.java
+++ kard-server/src/main/database/SQLite/SQLiteDataBaseFile.java
@@ -1,10 +1,14 @@
 package database.SQLite;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
 
 /**
  * A wrapper class for the handling of an SQLite database connection from a file.
+ *
+ * @see SQLiteDataBase
  */
 public class SQLiteDataBaseFile extends SQLiteDataBase {
     private String path;
@@ -15,15 +19,8 @@ public class SQLiteDataBaseFile extends SQLiteDataBase {
      * @param path The file path to the SQLiteDataBase
      * @throws FileNotFoundException if the file specified by the path does not exist.
      */
-    public SQLiteDataBaseFile(String path, boolean createIfNotExists) throws FileNotFoundException, SQLException{
-        this.path = path;
-        if(dbNotExists() && !createIfNotExists){
-            throw new FileNotFoundException(
-                    "Could not open SQLite database, the file located at "
-                            + this.path + " does not exist."
-            );
-        }
-
+    public SQLiteDataBaseFile(String path, boolean createIfNotExists) throws IOException, SQLException{
+        setPath(path, createIfNotExists);
         open();
     }
 
@@ -38,6 +35,16 @@ public class SQLiteDataBaseFile extends SQLiteDataBase {
         return !f.exists();
     }
 
+    /**
+     * Create the directory at path, if it doesn't already exist.
+     */
+    private void makeDbPathIfNotExists() throws IOException {
+        Path pathToDB = new File(path).toPath().normalize().getParent();
+
+        if (Files.exists(pathToDB)) return;
+        Files.createDirectory(pathToDB);
+    }
+
     @Override
     public void open() throws SQLException {
         connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s", path));
@@ -47,23 +54,30 @@ public class SQLiteDataBaseFile extends SQLiteDataBase {
      * Sets the path for the database.
      *
      * @param path The file path to the SQLiteDataBase
+     * @throws IOException if the file does not exist and createIfNotExists is false
+     * @throws IOException if the directory for the file does not exist and could not be created
      */
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    /**
-     *
-     * @param path The file path to the SQLiteDataBase
-     */
-    public void reopen(String path, boolean createIfNotExists) throws FileNotFoundException, SQLException {
-        setPath(path);
+    public void setPath(String path, boolean createIfNotExists) throws IOException {
         if(dbNotExists() && !createIfNotExists){
             throw new FileNotFoundException(
                     "Could not open SQLite database, the file located at "
                             + this.path + " does not exist."
             );
         }
+        makeDbPathIfNotExists();
+
+        this.path = path;
+    }
+
+    /**
+     *  Reopen the database to a new file.
+     *
+     * @param path The file path to the SQLiteDataBase
+     * @throws IOException if the file does not exist and createIfNotExists is false
+     * @throws IOException if the directory for the file does not exist and could not be created
+     */
+    public void reopen(String path, boolean createIfNotExists) throws IOException, SQLException {
+        setPath(path, createIfNotExists);
 
         reopen();
     }
