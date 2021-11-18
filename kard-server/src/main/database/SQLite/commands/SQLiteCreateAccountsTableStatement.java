e78451943f49c648223f32fc782e12cd73852dcf
Stewart Chandler <stewart.chandler@student.tdsb.on.ca>
2021-11-18 11:51:11 -0500

    added a statement for creating the accounts table

--- kard-server/src/main/database/SQLite/commands/SQLiteCreateAccountsTableStatement.java
+++ kard-server/src/main/database/SQLite/commands/SQLiteCreateAccountsTableStatement.java
@@ -1,2 +1,21 @@
-package database.SQLite.commands;public class SQLiteCreateAccountsTableStatement {
+package database.SQLite.commands;
+
+/**
+ * Creates the accounts table for the mainframe
+ *
+ * @see SQLiteStatement
+ */
+public class SQLiteCreateAccountsTableStatement extends SQLiteStatement{
+
+    /**
+     * Creates a statement to update the MainFrame with adding the accounts table if it doesn't exist
+     */
+    public SQLiteCreateAccountsTableStatement() {
+        this.statement = """
+                    CREATE TABLE IF NOT EXISTS "accounts" (
+                    	"username"	TEXT NOT NULL UNIQUE,
+                    	"password"	TEXT NOT NULL,
+                    	"account"   BLOB
+                    );""";
+    }
 }
