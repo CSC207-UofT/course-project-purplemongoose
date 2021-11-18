3ea6bc42f29c6944958ab4e99aa1530c3d53be35
Stewart Chandler <stewart.chandler@student.tdsb.on.ca>
2021-11-18 11:50:01 -0500

    added the SQLiteQuery class

--- kard-server/src/main/database/SQLite/commands/SQLiteQuery.java
+++ kard-server/src/main/database/SQLite/commands/SQLiteQuery.java
@@ -1,2 +1,33 @@
-package database.SQLite.commands;public class SQLiteQuery {
+package database.SQLite.commands;
+
+import database.SQLite.SQLiteDataBase;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * A class describing an SQL statement to call and update to the database.
+ */
+public abstract class SQLiteQuery extends SQLiteStatement{
+    private ResultSet results;
+
+    /**
+     * Execute a query and store the results.
+     *
+     * @param dataBase the database to execute the statement on.
+     * @throws SQLException if there was an error executing the query
+     */
+    @Override
+    public void execute(SQLiteDataBase dataBase) throws SQLException {
+        dataBase.executeQuery(statement, args);
+    }
+
+    /**
+     * Get the results of the query into the database
+     *
+     * @return the results of the query
+     */
+    public ResultSet getResults() {
+        return results;
+    }
 }
